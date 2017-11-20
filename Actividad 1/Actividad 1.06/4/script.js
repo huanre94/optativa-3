9ffbbfcc3425aa99d6085286f4d5647ac3415ec7
Hugo Restrepo <huanre94@gmail.com>
2017-11-20 17:57:26 -0500

    Ejercicio 4 terminado, falta 1 literal

--- Actividad 1/Actividad 1.06/4/script.js
+++ Actividad 1/Actividad 1.06/4/script.js
@@ -2,8 +2,10 @@ window.onload = function traerDatos() {
     var res = "";
     res = res + "Numero de elementos <p> que hay en el documento: " + document.getElementsByTagName("p").length + "\n";
     res = res + "Numero de enlaces existen en la pagina: " + document.getElementsByTagName("a").length + "\n";
-    res = res + "La dirección del ultimo enlace: " + document.location + "\n";
-    res = res + "El tamaño y color del párrafo de clase conclusion: " + +"\n";
+    res = res + "La dirección del ultimo enlace: " + window.history.back() + "\n";
+    res = res + "Numero de enlaces que hay en el segundo parrafo: " + document.getElementsByTagName("p").item(1).getElementsByTagName('a').length + "\n";
+    res = res + "El color de fondo de la pagina: " + document.body.style.backgroundColor + "\n";
+    res = res + "El tama\u00f1o y color del parrafo de clase conclusion: " + "Error" + "\n";
     res = res + "El titulo de la pagina: " + document.title + "\n";
     return alert(res);
 }
